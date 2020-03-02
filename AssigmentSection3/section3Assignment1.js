fe0616fd1096d2e86691d411955dc4d428ef5491
Bhavani <bhavani.donthineni@gmail.com>
2020-03-02 08:09:14 +0530

    section3

---
+++ AssigmentSection3/section3Assignment1.js
@@ -0,0 +1,10 @@
+
+// Converting from celsius temperature to fahrenheit temperature
+
+var kelvinTemp=301;
+
+var celsiusTemp=kelvinTemp-273.15;
+
+var fahreinheit =celsiusTemp+(9/5)+32;
+console.log("kelvin temperature is :"+kelvinTemp)
+console.log("for celsius temp "+celsiusTemp+" fahreinheit is "+fahreinheit);
