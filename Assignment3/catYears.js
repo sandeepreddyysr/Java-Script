9cc5a3c960d42cfe26f188855a1087c1bfe51008
sandeepreddyysr <sandeepreddyyerram@gmail.com>
2020-03-02 10:31:06 +0530

    Section3

---
+++ Assignment3/catYears.js
@@ -0,0 +1,16 @@
+// Convert Your Age to Cat Years
+var myAge = 23;
+ 
+var earlyYears = 2;
+ 
+earlyYears *= 24;
+ 
+var laterYears = myAge -2;
+ 
+laterYears *= 4;
+ 
+var myAgeInCatYears = earlyYears + laterYears;
+ 
+var myName = 'Sandeep Reddy';
+ 
+console.log('My name is ' + myName + '. I am ' + myAge + ' years old in human years which is ' + myAgeInCatYears + ' years old in cat years.');
